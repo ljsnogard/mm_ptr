5409d27136cfa3484f7da4d99d2c6b3a1db947df
lino <lino_snsalias@outlook.com>
2025-01-10 19:01:21 +0800

    Minor updates;

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,9 @@
 ﻿#![no_std]
 
 #![feature(alloc_layout_extra)]
-#![feature(const_alloc_layout)]
 #![feature(layout_for_ptr)]
 #![feature(slice_ptr_get)]
 #![feature(try_trait_v2)]
-
-// To allow a struct implement Fn*
-// #![feature(unboxed_closures)]
-// #![feature(fn_traits)]
 
 // We always pull in `std` during tests, because it's just easier
 // to write tests when you can assume you're on a capable platform
